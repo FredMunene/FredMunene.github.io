c698531db2e9b08b546217a80621cc9c68afed83
Fred <77734500+FredMunene@users.noreply.github.com>
2024-01-20 17:57:35 +0300

    adding event handler for changing themes

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-'use strict' ;
+'use strict' ; //provide better errors
+const switcher = document.querySelector('.btn') //variable switcher as reference to the button: 
+                 //document.queryselector handles css selectors
+switcher.addEventListener('click',function(){ //adding the event handler for click event; Listener for the 'click' event
+    document.body.classList.toggle('light-theme'); //toggle method modify 'body' element's class attribute
+    document.body.classList.toggle('dark-theme');
+
+    //to update the label of the button with the correct theme
+    //if theme is Light, label reads "Dark", else "Light"
+    const className = document.body.className;
+    if(className == "light-theme"){ 
+        this.textContent = "Dark";
+    } else  {
+        this.textContent = "Light";
+    }
+    //concole messages to help see results of code
+    console.log('Fred: ' + className)
+});
